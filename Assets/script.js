e62bfb5117f967a3390abbd2a329bbf0e24daf8f
Chris Kratz <Ckratz17@gmail.com>
2022-08-21 16:14:00 -0400

    trying different methods to display the questions and answers from the array onto the screen

--- Assets/script.js
+++ Assets/script.js
@@ -3,16 +3,16 @@ var highScorePage = document.getElementById('high_score_page')
 var startBtn = document.getElementById('startBtn')
 var intro = document.getElementById('intro')
 var questionPage = document.getElementById('question_page')
-var askQuestion = document.getElementById('ask-question')
-var choiceBtn = document.getElementById('choices')
-var answerA = document.getElementById('answer_btn1')
-var answerB = document.getElementById('answer_btn2')
-var answerC = document.getElementById('answer_btn3')
-var answerD = document.getElementById('answer_btn4')
+var askQuestion = document.querySelector('#ask-question')
+var choiceBtn = document.querySelector('.choices')
+var answerA = document.querySelector('.answerA')
+var answerB = document.querySelector('.answerB')
+var answerC = document.querySelector('.answerC')
+var answerD = document.querySelector('.answerD')
 var checkLine = document.getElementById('check_line')
 var submit = document.getElementById('submit_page')
 var finalScore = document.getElementById('final_score')
-var intials = document.getElementById('initials')
+var initials = document.getElementById('initials')
 var submitBtn = document.getElementById('submit_btn')
 var highScore = document.getElementById('high_score')
 var scoreCheck = document.getElementById('score')
@@ -22,56 +22,55 @@ var clearBtn = document.getElementById('clear_btn')
 
 var questions = [
     {
-        question:"",
-        choices: [],
-        correct: "",
+        question: "Question 1 : String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["quotes", "curly brackets", "commas"],
+        correct: "a",
     },
 
     {
-        question:"",
-        choices: [],
-        correct: "",
+        question: "Question 2 : Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "numbers", "alerts"],
+        correct: "d",
     },
 
     {
-        question:"",
-        choices: [],
-        correct: "",
+        question: "Question 3 : How do you create a function in JavaScript",
+        choices: ["function = myFunction()", "function myFunction()", "function:myFunction()", "create(function)"],
+        correct: "b",
     },
 
     {
-        question:"",
-        choices: [],
-        correct: "",
+        question: "Question 4 : How do you call a function named myFunction?",
+        choices: ["myFunction()", "function = myFunction()", "call function myFunction", "call myFunction()"],
+        correct: "a",
     },
 
     {
-        question:"",
-        choices: [],
-        correct: "",
+        question: "Question 5 : The first index of an array is _____.",
+        choices: ["0", "1", "10", "any"],
+        correct: "a",
     },
 
     {
-        question:"",
-        choices: [],
-        correct: "",
+        question: "Question 6 : To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["equals", "=", "==", "!="],
+        correct: "c",
     },
 
     {
-        question:"",
-        choices: [],
-        correct: "",
+        question: "Question 7 : How do you write an IF statement in JavaScript?",
+        choices: ["if i == 5 then", "if i = 5 then", "if (i == 5)", "if i = 5"],
+        correct: "c",
     },
 
     {
-        question:"",
-        choices: [],
-        correct: "",
+        question:"Questions 8 : Which event occurs when the user clicks on an HTML element?",
+        choices: ["onClick", "onChange", "onMouseOver", "onMouseClick"],
+        correct: "a",
     },
 ]
 
 var timeLeft = document.getElementById('timer')
-
 var secondsLeft = 60
 var questionNumber = 0
 var totalScore = 0
@@ -97,20 +96,19 @@ function countdown() {
 function startQuiz() {
     intro.style.display = 'none'
     questionPage.style.display = 'block'
-    questionNumber = 0
     countdown()
+    showQuestion()
 }
 
-startBtn.addEventListener('click', startQuiz)
-
 //show the questions
-function showQuestion() {
-    askQuestion.textContent = questions[n].question
-    answerA.textContent = questions[n].choices[0]
-    answerB.textContent = questions[n].choices[1]
-    answerC.textContent = questions[n].choices[2]
-    answerD.textContent = questions[n].choices[3]
-    questionNumber = n
+function showQuestion (id) {
+   if (id < questions.length) {
+    askQuestion.textContent = questions[id].question
+    answerA.textContent = questions[id].choices[0]
+    answerB.textContent = questions[id].choices[1]
+    answerC.textContent = questions[id].choices[2]
+    answerD.textContent = questions[id].choices[3]
+   }
 }
 
 //choose an answer
@@ -126,6 +124,7 @@ function selectAnswer(event) {
         checkLine.textContent = "Correct!"
         totalScore = totalScore + 1
     } else {
+        secondsLeft = secondsLeft -10
         checkLine.textContent = "Wrong! The correct answer is " + questions[questionNumber].correct + ' .'
     }
 
@@ -136,10 +135,6 @@ function selectAnswer(event) {
     }
     questionCount++
 }
-
-choiceBtn.forEach(function(click) {
-    click.addEventListener('click', selectAnswer)
-})
 
 //end the game and display the final score
 function gameOver() {
@@ -191,3 +186,41 @@ function saveScore () {
     renderScore()
 }
 
+startBtn.addEventListener('click', startQuiz)
+
+choiceBtn.forEach(function(click) {
+    click.addEventListener('click', selectAnswer)
+})
+
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault()
+    submit.style.display = 'none'
+    intro.style.display = 'none'
+    highScorePage.syle.display = 'block'
+    questionPage.style.display = 'none'
+    saveScore()
+})
+
+scoreCheck.addEventListener('click', function (event) {
+    event.preventDefault()
+    submit.style.display = 'none'
+    intro.style.display = 'none'
+    highScorePage.syle.display = 'block'
+    questionPage.style.display = 'none'
+    renderScore()
+})
+
+backBtn.addEventListener('click', function (event) {
+    event.preventDefault()
+    submit.style.display = 'none'
+    intro.style.display = 'none'
+    highScorePage.syle.display = 'block'
+    questionPage.style.display = 'none'
+    location.reload()
+})
+
+clearBtn.addEventListener('click', function (event) {
+    event.preventDefault()
+    localStorage.clear()
+    renderScore()
+})
